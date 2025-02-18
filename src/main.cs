face5cdbf355109ff0aaf1c628dd91fbec3ef9b6
forest <forestpw@outlook.com>
2025-02-18 09:26:57 -0700

    add echo command

--- src/main.cs
+++ src/main.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 
-var resume = true;
-while (resume) {
+var running = true;
+while (running) {
     Console.Write("$ ");
 
     var command = Console.ReadLine();
-    if (command == "exit") {
-        resume = false;
-        break;
+    switch (command) { 
+        case string subCommand when subCommand.Contains("exit"): 
+            running = false; 
+            break;
+        case string subCommand when subCommand.Contains("echo"):
+            command = command.Remove(0, 5);
+            Console.WriteLine($"{command}");
+            break;
+        default: 
+            Console.WriteLine($"{command}: command not found"); 
+            break;
     }
-    Console.WriteLine(command + ": command not found");
 }
