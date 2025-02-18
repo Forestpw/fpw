1c1fb8e28a191866837000fe0c37eed9f87c5967
forest <forestpw@outlook.com>
2025-02-18 09:39:36 -0700

    add type command

--- src/main.cs
+++ src/main.cs
@@ -6,12 +6,25 @@ while (running) {
 
     var command = Console.ReadLine();
     switch (command) { 
-        case string subCommand when subCommand.Contains("exit"): 
+        case string subCommand when subCommand.Contains("exit") && !subCommand.Contains("type"): 
             running = false; 
             break;
-        case string subCommand when subCommand.Contains("echo"):
+        case string subCommand when subCommand.Contains("echo") && !subCommand.Contains("type"):
             command = command.Remove(0, 5);
             Console.WriteLine($"{command}");
+            break;
+        case string subCommand when subCommand.Contains("type"):
+            var typeCommand = command.Remove(0, 5);
+            switch (typeCommand) {
+                case "exit":
+                case "type":
+                case "echo":
+                    Console.WriteLine("{0} is a shell builtin", typeCommand);
+                    break;
+                default:
+                    Console.WriteLine("{0}: not found", typeCommand);
+                    break;
+            }
             break;
         default: 
             Console.WriteLine($"{command}: command not found"); 
